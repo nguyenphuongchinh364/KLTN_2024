2b1bb551806e670e519c6410ea7d79a6c7d91254
Chinh Nguyen <nguyenphuongchinh364@gmail.com>
2024-03-11 19:33:15 +0700

    fix DoanhThu

--- src/main/java/vn/shopttcn/util/bean/DoanhThu.java
+++ src/main/java/vn/shopttcn/util/bean/DoanhThu.java
@@ -8,30 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DoanhThu {
-	public DoanhThu() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public DoanhThu(String month, long sales) {
-		super();
-		this.month = month;
-		this.sales = sales;
-	}
-
-	public String getMonth() {
-		return month;
-	}
-	public void setMonth(String month) {
-		this.month = month;
-	}
-	public long getSales() {
-		return sales;
-	}
-	public void setSales(long sales) {
-		this.sales = sales;
-	}
-
-	private String month;
-	private long sales;
-
+    private String month;
+    private long sales;
 }
